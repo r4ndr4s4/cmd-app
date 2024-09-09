7427edaee4daa2c8ba275b26ff06e3e9ed720e6c
András Rakita <Rakita.Andras@gmail.com>
2024-09-09 14:11:47 +0200

    feat: init cmd app prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,97 @@
+import styled from "@emotion/styled";
+import { useState, KeyboardEvent, useRef, useEffect } from "react";
+
+import { allowedInputKeys } from "./utils";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+
+  p {
+    margin: 0;
+  }
+
+  span {
+    animation: blink 1s step-end infinite;
+    border-bottom: 2px solid white;
+  }
+
+  @keyframes blink {
+    from,
+    to {
+      border-color: transparent;
+    }
+    50% {
+      border-color: #fff;
+    }
+  }
+`;
+
+const Input = styled.div`
+  padding-bottom: 10px;
+`;
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [input, setInput] = useState("");
+  const [history, setHistory] = useState<string[]>([]); // Array(65).fill("test")
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    containerRef.current?.focus();
+  });
+
+  useEffect(() => {
+    inputRef.current?.scrollIntoView();
+  });
+
+  const handleKeyUp = (e: KeyboardEvent<HTMLImageElement>) => {
+    if (!allowedInputKeys.includes(e.key)) {
+      return;
+    }
+
+    switch (e.key) {
+      case "Backspace":
+        setInput(input.slice(0, input.length - 1));
+
+        break;
+      case "Enter":
+        setHistory([...history, input]);
+        setInput("");
+
+        break;
+      default: {
+        const currentCommand = `${input}${e.key}`; // TODO input
+
+        setInput(currentCommand);
+
+        break;
+      }
+    }
+  };
+
+  // TODO destructure
+  return (
+    <Container onKeyUp={handleKeyUp} tabIndex={0} ref={containerRef}>
+      <p>Hello World!</p>
+
+      <div>
+        {
+          // TODO key
+          history.map((command, i) => (
+            <p key={i}>{command}</p>
+          ))
+        }
+      </div>
+
+      <Input ref={inputRef}>
+        {input}
+        <span>&nbsp;</span>
+      </Input>
+    </Container>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
 
+import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
