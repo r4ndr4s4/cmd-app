60b7dd9762ac25d304d277ca806badff0b225f1e
András Rakita <Rakita.Andras@gmail.com>
2024-09-20 16:36:01 +0200

    feat: memory test animation

--- src/components/Post/FirstScreen/HardwareInfo.tsx
+++ src/components/Post/FirstScreen/HardwareInfo.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+
+const MEMORY = 32768;
+
+const INITIAL_MEMORY = 640;
+const MEMORY_INCREMENT = INITIAL_MEMORY - 1;
+
 function HardwareInfo() {
+  const [testedMemory, setTestedMemory] = useState(INITIAL_MEMORY);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const increment = Math.min(MEMORY, testedMemory + MEMORY_INCREMENT);
+
+      if (increment === MEMORY) {
+        clearInterval(interval);
+      }
+
+      setTestedMemory(increment);
+    }, 25);
+
+    return () => clearInterval(interval);
+  }, [testedMemory]);
+
   return (
     <>
       <p>(55XWUQ0E) Intel i430VX PCIset(TM)</p>
       <br />
 
       <p>PENTIUM-S CPU at 75MHz</p>
-      <p>Memory Test: 32768K OK</p>
+      <p>Memory Test: {testedMemory}K OK</p>
     </>
   );
 }
