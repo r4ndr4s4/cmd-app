5c9670a236089ddaddb516d92c16cb219e95680e
András Rakita <Rakita.Andras@gmail.com>
2024-10-20 17:31:45 +0200

    chore: change texting on mobile

--- src/components/Post/FirstScreen/Footer.tsx
+++ src/components/Post/FirstScreen/Footer.tsx
@@ -1,11 +1,17 @@
+import useDetectTouchScreenDevice from "../../../hooks/useDetectTouchScreenDevice";
 import { ColoredSpan, LIGHT_TEXT_COLOR } from "../../../utils/styles";
 
 function Footer() {
+  const isTouchScreenDevice = useDetectTouchScreenDevice();
+
   return (
     <div>
       <p>
-        Press <ColoredSpan color={LIGHT_TEXT_COLOR}>DEL</ColoredSpan> to skip
-        POST
+        Press{" "}
+        <ColoredSpan color={LIGHT_TEXT_COLOR}>
+          {isTouchScreenDevice ? "ENTER" : "DEL"}
+        </ColoredSpan>{" "}
+        to skip POST
       </p>
       <p>12/10/97-i430VX,UMC8669-2A59GH2BC-00</p>
     </div>
