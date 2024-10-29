fc147fe8094af6e01f6cafe7b7530127bf596848
András Rakita <Rakita.Andras@gmail.com>
2024-10-29 23:56:47 +0100

    chore: removing react-img-mapper

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import ImageMapper from "react-img-mapper";
 import { useState } from "react";
 
 import siemensNixdorfPc from "../assets/siemens_nixdorf_pc.webp";
@@ -17,6 +16,11 @@ import useGetWindowWidth from "../hooks/useGetWindowWidth";
 export const IMG_WIDTH = 1577;
 const MININUM_WINDOW_WIDTH = 1366;
 
+const X1default = 729;
+const X2default = 806;
+const Y1default = 446;
+const Y2default = 487;
+
 const Container = styled.div<{ startFadeOut: boolean }>`
   display: flex;
   flex-direction: column;
@@ -27,21 +31,32 @@ const Container = styled.div<{ startFadeOut: boolean }>`
   animation: ${(props) =>
     props.startFadeOut ? "fade-out 0.25s ease-out both" : ""}; // TODO
 
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
   area {
     cursor: pointer;
   }
 `;
 
 function Start() {
-  const [imageMapLoaded, setImageMapLoaded] = useState(false);
+  const [X1, setX1] = useState(X1default);
+  const [X2, setX2] = useState(X2default);
+  const [Y1, setY1] = useState(Y1default);
+  const [Y2, setY2] = useState(Y2default);
 
   const postState = useStore((state) => state.postState);
 
   const isTouchScreenDevice = useDetectTouchScreenDevice();
   const windowWidth = useGetWindowWidth(() => {
-    if (window.innerWidth < MININUM_WINDOW_WIDTH) {
-      setImageMapLoaded(false);
-    }
+    const ratioNumber = Math.min(window.innerWidth, IMG_WIDTH) / IMG_WIDTH;
+
+    setX1(X1default * ratioNumber);
+    setX2(X2default * ratioNumber);
+    setY1(Y1default * ratioNumber);
+    setY2(Y2default * ratioNumber);
   });
 
   useDelayedPostStateChange({
@@ -90,47 +105,35 @@ function Start() {
       onClick={() => hiddenInputRef.current?.focus()}
     >
       <div>
-        <ImageMapper
-          src={siemensNixdorfPc}
-          map={{
-            name: "powerButton",
-            areas: [
-              {
-                shape: "rect",
-                coords: [729, 446, 806, 487],
-              },
-            ],
-          }}
-          parentWidth={Math.min(windowWidth, IMG_WIDTH)}
-          onLoad={() => setImageMapLoaded(true)}
-          onClick={() => {
-            // TODO play sound, CRT effect
-            setTimeout(() => {
-              useStore.setState(
-                () => ({
-                  postState: PostState.StartScreenDone,
-                }),
-                undefined,
-                {
-                  type: "postState",
-                  from: PostState.StartScreenInit,
-                  to: PostState.StartScreenDone,
-                }
-              );
-            }, 500);
-          }}
-          active={false}
-          responsive
-        />
+        <img src={siemensNixdorfPc} useMap="#workmap" />
+
+        <map id="workmap" name="workmap">
+          <area
+            shape="rect"
+            coords={`${X1.toString()}, ${Y1.toString()}, ${X2.toString()}, ${Y2.toString()}`}
+            onClick={() => {
+              // TODO play sound, CRT effect
+              setTimeout(() => {
+                useStore.setState(
+                  () => ({
+                    postState: PostState.StartScreenDone,
+                  }),
+                  undefined,
+                  {
+                    type: "postState",
+                    from: PostState.StartScreenInit,
+                    to: PostState.StartScreenDone,
+                  }
+                );
+              }, 500);
+            }}
+          />
+        </map>
 
         <HiddenInput type="text" ref={hiddenInputRef} />
       </div>
 
-      {imageMapLoaded ? (
-        <Tooltip windowWidth={windowWidth} />
-      ) : (
-        "Loading..." // TODO
-      )}
+      <Tooltip windowWidth={windowWidth} />
 
       <Notification>
         {isTouchScreenDevice
