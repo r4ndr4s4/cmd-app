cc175c9c5014adcdf06e44cb1514ade1aa6b32f6
András Rakita <Rakita.Andras@gmail.com>
2024-11-01 18:00:43 +0100

    Revert "chore: wip switch to google analytics"
    This reverts commit 11741e6450eebbb2e974ea62f42c6ac942592653.

--- src/hooks/useTrackComponentRender.ts
+++ src/hooks/useTrackComponentRender.ts
@@ -1,13 +1,11 @@
 import { useEffect } from "react";
 import { track } from "@vercel/analytics/react";
 
-import { analytics } from "../main";
-
 function useTrackComponentRender(
   component: "APP" | "START" | "POST" | "FIRST_SCREEN" | "SECOND_SCREEN" | "CMD"
 ) {
   useEffect(() => {
-    void analytics.track(`${component} rendered`);
+    track(`${component} rendered`);
   }, [component]);
 
   return { component, trackEvent: track };
